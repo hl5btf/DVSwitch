88b200764c2dc177b2e61f49659da3f82b93351c
hl5ky <64560125+hl5ky@users.noreply.github.com>
2020-11-16 20:46:21 +0900

    Update bashrc.sh

--- bashrc.sh
+++ bashrc.sh
@@ -2,9 +2,9 @@
 
 ### ~/.bashrc에 Alias 추가
 echo "---------------------ALIAS 추가"
-sudo wget -O /home/dvswitch/temp/bashrc.add https://raw.githubusercontent.com/hl5btf/DVSwitch/main/bashrc.add
-sudo cat /home/dvswitch/.bashrc /home/dvswitch/temp/bashrc.add > bash_new
-sudo rm /home/dvswitch/temp/bashrc.add
+sudo wget bashrc.add https://raw.githubusercontent.com/hl5btf/DVSwitch/main/bashrc.add
+sudo cat /home/dvswitch/.bashrc bashrc.add > bash_new
+sudo rm bashrc.add
 sudo mv bash_new /home/dvswitch/.bashrc
 
 sudo nano ~/.bashrc
