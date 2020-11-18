4ecd0c44951b38c950a70b5a5d7f0454bf7f919d
hl5ky <64560125+hl5ky@users.noreply.github.com>
2020-11-18 09:30:25 +0900

    Update config_2nd.sh

--- config_2nd.sh
+++ config_2nd.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
 ### ~/.bashrc에 Alias 추가
-echo "---------------------ALIAS 추가"
-sudo wget bashrc.add https://raw.githubusercontent.com/hl5btf/DVSwitch/main/bashrc.add
-sudo cat /home/dvswitch/.bashrc bashrc.add > bash_new
-sudo rm bashrc.add
-sudo mv bash_new /home/dvswitch/.bashrc
 
-# sudo nano /home/dvswitch/.bashrc
+file=/home/dvswitch/.bashrc
+text=Analog_Bridge
+
+if [[ -z `sudo grep $text $file` ]]; then
+  echo "---------------------ALIAS 추가"
+  sudo wget bashrc.add https://raw.githubusercontent.com/hl5btf/DVSwitch/main/bashrc.add
+  sudo cat $file bashrc.add > bash_new
+  sudo rm bashrc.add
+  sudo mv bash_new $file
+fi
+
+# sudo nano $file
