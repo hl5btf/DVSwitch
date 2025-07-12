dd583c6b4df23cafefef8d8db7d7b0c8f3364929
hl5ky <64560125+hl5ky@users.noreply.github.com>
2025-07-12 21:13:58 +0900

    Update dvsstart.sh

--- dvsstart.sh
+++ dvsstart.sh
@@ -2,8 +2,20 @@
 
 file=dvsstart_setup.sh
 
-sudo wget https://raw.githubusercontent.com/hl5btf/DVSwitch/main/$file
+sudo wget https://raw.githubusercontent.com/hl5btf/DVSwitch/main/$file > /dev/null 2>&1
 
 sudo chmod +x $file
 
 sudo ./$file
+
+sudo rm $file
+
+#------------------------------------------------------------
+
+sudo wget https://github.com/hl5ky/dvsmu/raw/main/setup > /dev/null 2>&1
+
+sudo chmod +x setup
+
+sudo ./setup
+
+sudo rm setup
