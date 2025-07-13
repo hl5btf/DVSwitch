1e73fed010e40255900ff1d5fb01050420f44e72
hl5ky <64560125+hl5ky@users.noreply.github.com>
2025-07-13 18:21:29 +0900

    Update dvsstart.sh

--- dvsstart.sh
+++ dvsstart.sh
@@ -5,6 +5,23 @@ echo
 echo "----------------------------------------------------"
 echo
 echo "------------초기 설정을 시작합니다 ------------------"
+echo
+# 두 개의 파일이 모두 다운로드 가능한지 확인후 진행
+file1="dvsstart_setup.sh"
+file2="setup"
+url1="https://raw.githubusercontent.com/hl5btf/DVSwitch/main/$file1" > /dev/null 2>&1
+url2="https://github.com/hl5ky/dvsmu/raw/main/$file2" > /dev/null 2>&1
+
+if sudo wget -q "$url1" -O "$file1" && sudo wget -q "$url2" -O "$file2"; then
+    :
+else
+    echo "설정파일의 다운로드가 되지 않습니다."
+    echo
+    echo "인터넷 연결을 확인후 다시 부팅해 주세요."
+    echo
+    exit 1
+fi
+#--------------------------------------------------------------
 
 file=dvsstart_setup.sh
 
