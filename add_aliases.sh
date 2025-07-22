a60d2eee55d1afead2ebdff48c89b83b3b428c5f
hl5ky <64560125+hl5ky@users.noreply.github.com>
2025-07-22 12:08:22 +0900

    Update add_aliases.sh

--- add_aliases.sh
+++ add_aliases.sh
@@ -7,6 +7,7 @@ read -r -d '' ALIASES << 'EOF'
 alias dv="cd /usr/local/dvs"
 alias data="cd /var/lib/dvswitch/dvs"
 alias adv="cd /var/lib/dvswitch/dvs/adv"
+alias user="cd /var/lib/dvswitch/dvs/adv"
 alias lan="cd /var/lib/dvswitch/dvs/lan"
 alias tgdb="cd /var/lib/dvswitch/dvs/tgdb"
 alias ab="cd /opt/Analog_Bridge"
