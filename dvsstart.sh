4a6c794e3bba5d7d398df6a0b48792993b8cf354
hl5ky <64560125+hl5ky@users.noreply.github.com>
2025-08-01 20:52:14 +0900

    Update dvsstart.sh

--- dvsstart.sh
+++ dvsstart.sh
@@ -63,7 +63,7 @@ fi
 #==============================================================
 
 # dvswitch_upgrade + dvsmu_upgrade (로그기록은 하지 않도록)
-sudo DISABLE_LOG=1 /usr/local/dvs/auto_upgrade.sh
+sudo env DISABLE_LOG=1 /usr/local/dvs/auto_upgrade.sh
 
 # dvsconfig.txt에 설정한 항목을 이용하여 초기 설정을 자동으로 하는 루틴
 run_dvsconfig
